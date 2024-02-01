dfb14746c94cf065499a745f2fa3c0ebf6d55842
Viswanath Chidambaram <vc@abstractit.in>
2024-02-01 21:05:38 +0530

    Refactored.

--- ashtakavarga.py
+++ ashtakavarga.py
@@ -10,6 +10,11 @@ ashtakavargaBufferDF = pd.DataFrame(
 		product(signs, planets, ashtaka), columns=['sign', 'object', 'reference']
 	)
 
+columnRenameMap = {
+	1: 'h1', 2: 'h2', 3: 'h3', 4: 'h4', 5: 'h5', 6: 'h6',
+	7: 'h7', 8: 'h8', 9: 'h9', 10: 'h10', 11: 'h11', 12: 'h12',
+}
+
 def getAshtakavarga(config):
 	objects = getObjects(config)
 	ashtakaObjects = selectObjects(objects, ashtaka)
@@ -32,7 +37,7 @@ def getAshtakavarga(config):
 		house=lambda row: (houseCount + row['sign'] - ascSign) % houseCount + 1
 	)\
 	.merge(ashtakavargaBaseDF, on=['object', 'reference', 'position'])\
-	.groupby('house')['points'].sum().to_dict()
+	.groupby('house')['points'].sum().rename(columnRenameMap)
 
 	return ashtakavargaDF
 
