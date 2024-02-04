d61337bf35ee024c6237352179a1bc2916250ff3
Viswanath Chidambaram <vc@abstractit.in>
2024-02-05 05:04:25 +0530

    Set up basic querying for muhurta yogas.

--- panchang/getMuhurtaYoga.py
+++ panchang/getMuhurtaYoga.py
@@ -1,6 +1,23 @@
+import pandas as pd
+from core.helpers import (
+	resolveRelativePath,
+)
+
+muhurtaYogaDF = pd.read_csv(
+	resolveRelativePath(
+		'./muhurtaYogaEffects.csv'
+	)
+)
+
+
 def getMuhurtaYoga(panchang):
-	return (
-		panchang.tithi,
-		panchang.vaara,
-		panchang.nakshatra,
+	vaara = panchang.vaara
+	tithi = panchang.tithi
+	nakshatra = panchang.nakshatra
+
+	return muhurtaYogaDF.query(
+		'tithi == @tithi and vaara == @vaara or '
+		+ 'tithi == @tithi and nakshatra == @nakshatra or '
+		+ 'vaara == @vaara and nakshatra == @nakshatra or '
+		+ 'tithi == @tithi and vaara == @vaara and nakshatra == @nakshatra'
 	)
