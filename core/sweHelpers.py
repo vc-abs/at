6cb17a588998bb6c94bdb2141e3ad7e742d1f28b
Viswanath Chidambaram <vc@abstractit.in>
2024-07-16 04:09:51 +0530

    Introduced some SwE helpers.

--- core/sweHelpers.py
+++ core/sweHelpers.py
@@ -1,8 +1,11 @@
-import swisseph as swe
 from datetime import (
 	datetime,
 	timezone,
 	timedelta,
+)
+import swisseph as swe
+from sideralib import (
+	astrodata,
 )
 
 gregflag = swe.GREG_CAL
@@ -19,6 +22,7 @@ utcTimeZone = timezone(
 
 
 def floatToHMS(floatHours):
+	# #TODO: Fix the minor inconsistency (of 1 second), happening probably due to difference in floating point operations, when the timezone is +05:30.
 	hours = int(floatHours)
 	remainingHours = floatHours - hours
 	minutes = int(remainingHours * 60)
@@ -30,11 +34,11 @@ def floatToHMS(floatHours):
 	return hours, minutes, seconds
 
 
-def parseSweTime(
-	tret, tzinfo=utcTimeZone
+def jdtToDateTime(
+	fromTret, fromTZInfo=utcTimeZone
 ):
 	year, month, day, floatHour = (
-		swe.revjul(tret[0], gregflag)
+		swe.revjul(fromTret, gregflag)
 	)
 
 	hour, min, sec = floatToHMS(floatHour)
@@ -47,7 +51,23 @@ def parseSweTime(
 		min,
 		sec,
 		tzinfo=utcTimeZone,
-	).astimezone(tzinfo)
+	).astimezone(fromTZInfo)
+
+
+def dateTimeToJDT(fromValue):
+	offsetSeconds = (
+		fromValue.utcoffset()
+		or datetime.now(
+			utcTimeZone
+		).utcoffset()
+	).seconds
+
+	return swe.julday(
+		fromValue.year,
+		fromValue.month,
+		fromValue.day,
+		(offsetSeconds / 3600),
+	)
 
 
 # #NOTE: There's some one minute difference in both sunrise and sunset times, when compared to JH. The flags used are to get closer to those values, and are not from proper understanding.
@@ -103,8 +123,49 @@ def getRiseAndSetTimes(config):
 	return {
 		'sunrise': None
 		if isRiseMissing
-		else parseSweTime(tRise, dt.tzinfo),
+		else jdtToDateTime(
+			tRise[0], dt.tzinfo
+		),
 		'sunset': None
 		if isSetMissing
-		else parseSweTime(tSet, dt.tzinfo),
+		else jdtToDateTime(
+			tSet[0], dt.tzinfo
+		),
 	}
+
+
+# #NOTE: This function is not actively used, but was kept as it seems useful and it incurred quite a chunk of effort.
+def getAyanamsaOffset(
+	forTime=None, ayanamsaName=None
+):
+	forTime = forTime or datetime.now()
+	swe.set_sid_mode(swe.SIDM_RAMAN, 0, 0)
+
+	return swe.get_ayanamsa_ex(
+		swe.julday(
+			2024,
+			1,
+			1,
+		),
+		0,
+	)[1]
+
+
+# #NOTE: This function is not actively used, but was kept as it seems useful and it incurred quite a chunk of effort.
+def getSolarCrossingTime(
+	longitude, startDate
+):
+	# #TODO: Fix the following bug. The Ayanamsa is hardcoded, instead of being passed as a param, as the proper way to implement is not known (even to get the current value).
+	swe.set_sid_mode(swe.SIDM_RAMAN, 0, 0)
+	jdStartDate = dateTimeToJDT(startDate)
+
+	jdCross = swe.solcross(
+		longitude,
+		jdStartDate,
+		swe.FLG_SIDEREAL
+		| swe.FLG_CENTER_BODY,
+	)
+	return jdtToDateTime(
+		jdCross,
+		startDate.tzinfo or utcTimeZone,
+	)
