1adac7c6c0e15a24ae361be3ed71fdd7f0a0c17a
Viswanath Chidambaram <vc@abstractit.in>
2024-07-27 06:34:30 +0530

    Cleaned up some unused code.

--- chart/__init__.py
+++ chart/__init__.py
@@ -21,68 +21,6 @@ from core.sweHelpers import (
 from core.Cached import Cached
 
 
-def formatPlanet(planetName, planet):
-	sign = planet['lon'] // signWidth + 1
-	return {
-		'name': planetName,
-		'type': 'planet',
-		'longitude': planet['lon'],
-		'retrograde': planet['retrograde'],
-		'sign': sign,
-	}
-
-
-def formatPlanets(planets):
-	return [
-		formatPlanet(planetName, planet)
-		for (
-			planetName,
-			planet,
-		) in planets.items()
-	]
-
-
-def formatHouse(houseNum, house):
-	traditionalObjects = selectObjects(
-		formatPlanets(house.planets),
-		objects,
-	)
-
-	return {
-		'house': houseNum,
-		'sign': house.sign_num,
-		'objects': traditionalObjects,
-	}
-
-
-def addAscendant(kundli, houses):
-	firstKundliHouse = kundli[0]
-	houses[0]['objects'].insert(
-		0,
-		{
-			'name': 'asc',
-			'type': 'angle',
-			'longitude': firstKundliHouse.asc_lon,
-			'sign': firstKundliHouse.sign_num,
-		},
-	)
-
-
-def enrichObject(object, house):
-	return {
-		**object,
-		'sign': house['sign'],
-		'house': house['house'],
-	}
-
-
-def getObjectsFromHouse(house):
-	return [
-		enrichObject(object, house)
-		for object in house['objects']
-	]
-
-
 class Chart(Cached):
 	def __init__(self, config):
 		super().__init__()
